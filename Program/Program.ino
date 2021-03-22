f65233026534e65c68b541005def235bd7945924
Riemersma,S.N. (Sietze) <s.n.riemersma@students.uu.nl>
2021-03-22 18:00:47 +0100

    Servo + D fix

--- Program/Program.ino
+++ Program/Program.ino
@@ -1,22 +1,37 @@
 #include <ESP8266WiFi.h>
 #include <Servo.h>
+#include <Wire.h> 
+#include "SSD1306Wire.h"
+#include "OLEDDisplayUi.h"
 
-<<<<<<< HEAD
+#define D0 16
+#define D1 5
+#define D2 4
+#define D3 0
+#define D4 2
+#define D5 14
+#define D6 12
+#define D7 13
+#define D8 15
+
+
+#define SERVO_PIN D0
 #define ONBOARD_LED 16
 #define ESP8266_LED 2
 #define MOISTURE_LED A0
-=======
-#define ONBOARD_LED 16    // LED on the side of the processor
-#define ESP8266_LED 2     // LED on the side of the wifi unit
 
-// ADC_MODE(ADC_VCC); will result in compile error. use this instead.
-int __get_adc_mode(void) { return (int) (ADC_VCC); }
->>>>>>> 20994ed62fb850912f7a80b89bd74c796258cca4
+Servo servo;
+SSD1306Wire  display(0x3c, D3, D5);
+
+OLEDDisplayUi ui ( &display );
+int screenW = 128;
+int screenH = 64;
 
 void setup() 
 {
   pinMode(ONBOARD_LED, OUTPUT);
   pinMode(ESP8266_LED, OUTPUT);
+  servo.attach(SERVO_PIN);
   Serial.begin(115200);
 
   // Print Wi-Fi Information
@@ -29,6 +44,5 @@ void setup()
 
 void loop() 
 {
-  Serial.println(analogRead(MOISTURE_LED));
-  delay(500);
+  
 }
