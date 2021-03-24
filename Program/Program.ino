a9238fb4ea5d9ffadbd91c37772fd712f3779e45
Wal,K.A. van der (Karst) <k.a.vanderwal@students.uu.nl>
2021-03-24 17:48:07 +0100

    merge

--- Program/Program.ino
+++ Program/Program.ino
@@ -1,17 +1,9 @@
 #include <ESP8266WiFi.h>
 #include <Servo.h>
 
-<<<<<<< HEAD
-#define ONBOARD_LED 16
-#define ESP8266_LED 2
-#define MOISTURE_LED A0
-=======
 #define ONBOARD_LED 16    // LED on the side of the processor
 #define ESP8266_LED 2     // LED on the side of the wifi unit
-
-// ADC_MODE(ADC_VCC); will result in compile error. use this instead.
-int __get_adc_mode(void) { return (int) (ADC_VCC); }
->>>>>>> 20994ed62fb850912f7a80b89bd74c796258cca4
+#define MOISTURE_LED A0
 
 void setup() 
 {
